85026ebcf809c53a773013ecad88d4204385aa9a
Akash Saxena <akashsaxena052002@gmail.com>
2026-04-12 21:16:42 +0530

    dom issue done

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  root: 'client',
   plugins: [react()],
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  }
 })
